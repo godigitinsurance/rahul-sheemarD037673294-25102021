63d28c1ac095793a2f309c667f04d10b19aba890
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2022-10-27 19:23:04 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-$("#customerName").text("Rahul Sheehmar");
+$("#customerName").text("Shiv Kumar");
 $("#policyNumber").text("D037673294 / 25102021");
-$("#startDate").text("21-Oct-2021");
-$("#endDate").text("20-Oct-2022");
+$("#startDate").text("21-Jul-2021");
+$("#endDate").text("20-Jul-2022");
 
 
 $("#type").text("Two Wheeler");
-$("#registrationNumber").text("PB08EB6271");
-$("#make").text("ROYAL ENFIELD");
-$("#model").text("BULLET 350");
-$("#vehicalSubType").text("UCE");
-$("#engineNo").text("U3K5C1JF399706");
-$("#chassis").text("ME3U3K5C1JF335916");
+$("#registrationNumber").text("PB08CZ5618");
+$("#make").text("BAJAJ");
+$("#model").text("DISCOVER 125M");
+$("#vehicalSubType").text("DRUM BRAKE");
+$("#engineNo").text("PAZRFK99083");
+$("#chassis").text("MD2A57BZXFRK78669");
